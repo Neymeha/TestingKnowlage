031b562cb505fad91fd86c2b043388086f5ef33c
Herman Stepanov <neymeha@inbox.ru>
2023-05-22 09:19:46 +0300

    some comments

--- src/main/java/com/neymeha/myadminclientapplication/test/collections/stream_api/CreateData.java
+++ src/main/java/com/neymeha/myadminclientapplication/test/collections/stream_api/CreateData.java
@@ -8,10 +8,13 @@ import java.util.stream.Collectors;
 public class CreateData {
     private static final Faker faker = new Faker();
     private static final Random random = new Random();
+
+    //как то так сделал метод для создания списка департаментов
     public static List<Department> createDepartmentsList(String...name){
         return Arrays.stream(name).map(n->new Department(n)).collect(Collectors.toList());
     }
 
+    //как то так сделал список рандомных персон для добавления в список департаментов
     public static List<Person> createRandomPersonsList(int count){
         return Arrays.stream(new Person[count]).map(p->new Person(faker.name().firstName(),faker.name().lastName(),
                 faker.number().numberBetween(20,50),faker.company().profession(),
--- src/main/java/com/neymeha/myadminclientapplication/test/collections/stream_api/Test.java
+++ src/main/java/com/neymeha/myadminclientapplication/test/collections/stream_api/Test.java
@@ -8,24 +8,35 @@ import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
+        // создаем обьекты для работы с ними через Stream API(создаем в утилитарном классе с помощью Stream API)
         List<Department> depList=CreateData.createDepartmentsList("HR","IT","MANAGEMENT","SALES");
         List<Person> perList1=CreateData.createRandomPersonsList(10);
         List<Person> perList2=CreateData.createRandomPersonsList(10);
 
+        // конкатенация стримов и дальнешая фильтрация с целью добавления отфильтрованных и обьединенных данных в коллекцию
         depList.get(0).setDepartmentWorkers(Stream.concat(perList1.stream(),perList2.stream()).filter(p->p.sex==true).
                 collect(Collectors.toList()));
+        // сортировка
         depList.get(1).setDepartmentWorkers(perList1.stream().sorted().collect(Collectors.toList()));
+        // тут что то пошло не так, хотел добавить в коллекцию список работников с заполненным полем Департамента, но
+        // вызвав метод в .map() все покраснело, не смог разобарться
         depList.get(2).setDepartmentWorkers(perList2.stream()
                 .map(p->new Person("p.getName()","p.getSurname()",p.getAge(),p.getOccupation(),p.getSalary(),p.isSex()))
                 .collect(Collectors.toList())); //почему в мап я не смог вызвать метод .setDepartment("Management") ????
 
+        // находим первого работника с фильтрацией, чисто тестировал Optional
         Optional<String> worker = perList1.stream().
                 filter(person -> person.getAge()<30).map(person -> person.getName()+" "+person.getSurname()).findFirst();
+        // Среднюю зарпалту высчитываю среди всех работников находящихся в коллекции, пытался сначала перевести все
+        // в double stream через mapToDouble, там тоже есть метод average(), но что то пошло не так. В итоге не разобрался
+        // и остановился на таком варианте
         Double averageSalary=depList.stream()
                 .flatMap(department -> department.getDepartmentWorkers().stream())
                 .collect(Collectors.averagingDouble(p->p.getSalary()));
 
+        // группировка в мапу по данным
         Map <Integer, List <Person>> personByAge=perList1.stream().collect(Collectors.groupingBy(p->p.getAge()));
+        // группировка в мапу по логике
         Map <Boolean, List <Person>> personBySex=Stream.concat(perList1.stream(),perList2.stream())
                         .collect(Collectors.partitioningBy(person -> person.isSex()==true));
 
